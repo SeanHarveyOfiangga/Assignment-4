072c6b2006724efd4079d93e4e363b1268b1a99a
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-11-20 16:22:22 +0800

    Program revised

--- Name_Age_Address-V3.py
+++ Name_Age_Address-V3.py
@@ -1,8 +1,6 @@
-def GetInfo (Name, Age, Address):
-    print(f"Hi, my name is {Name}. I am {Age} years old and I live in {Address}.")
-
-Name = input("Enter your Name: ")
-Age = input("Enter your Age: ")
-Address = input("Enter your Address: ")
-
-GetInfo (Name, Age, Address) 
+def GetInfo ():
+    Name = input("Enter your Name: ")
+    Age = input("Enter your Age: ")
+    Address = input("Enter your Address: ")
+    return print(f"Hi, my name is {Name}. I am {Age} years old and I live in {Address}.")
+GetInfo ()
